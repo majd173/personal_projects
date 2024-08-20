052541d6d4055aea9289a68564d5ae9646491465
majd173 <majdb173@gmail.com>
2024-08-20 20:28:32 +0300

    Horfiesh Jobs.

--- oop_projects/hurfiesh_jobs/tests/test_add_job.py
+++ oop_projects/hurfiesh_jobs/tests/test_add_job.py
@@ -11,10 +11,10 @@ class TestAddJob(unittest.TestCase):
     def setUp(self):
         base_dir = os.path.dirname(os.path.abspath(__file__))
         self._config_file_path = os.path.join(base_dir, '..\horfiesh.json')
-        self._config = ConfigProvider()
+        self._config = ConfigProvider().load_from_file(self._config_file_path)
 
     def test_add_only_job(self):
-        job_title = Utilities.generate_random_string_with_punctuation(10)
+        job_title = Utilities.generate_random_string_only_letters(10)
         job = Job(job_title)
         job.add_job(None)
-        self.assertIn(self._config_file_path)
+        self.assertIn(self._config['jobs'][0]['title'], job_title)
