9bddbfacd4c71051c5b9a0bfcd70d4be66404e55
majd173 <majdb173@gmail.com>
2024-10-31 17:28:46 +0200

    web testing

--- web_testing_projects/api_request/api_request.py
+++ web_testing_projects/api_request/api_request.py
@@ -1,6 +1,16 @@
 import requests
+
 url = "https://www.selenium.dev/about/"
 
 response = requests.get(url)
 print(response.status_code)
 print(response.ok)
+
+# Another example
+url = "https://fakerestapi.azurewebsites.net/api/v1/Activities"
+
+response = requests.get(url)
+response_data = response.json()
+print(response_data)
+
+
