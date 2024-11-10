492b1cbd3985bac6029969937ff93317b46ae311
majd173 <majdb173@gmail.com>
2024-11-10 20:54:38 +0200

    web testing

--- web_testing_projects/api_request/api_request_example.py
+++ web_testing_projects/api_request/api_request_example.py
@@ -1,17 +1,46 @@
 import requests
 
-url = "https://www.ynet.co.il/iphone/json/api/talkbacks/add"
 
-data = {
-  "talkback_title": "ישמור",
-  "talkback_content": "111",
-  "author_name": "ישראלי",
-  "author_email": "",
-  "author_location": "",
-  "article_id": "sy1ufcszyg",
-  "talkbackParentData": "",
-  "talkback_source": None
-}
+#
+# url = "https://www.ynet.co.il/iphone/json/api/talkbacks/add"
+#
+# data = {
+#     "talkback_title": "ישמור",
+#     "talkback_content": "111",
+#     "author_name": "ישראלי",
+#     "author_email": "",
+#     "author_location": "",
+#     "article_id": "sy1ufcszyg",
+#     "talkbackParentData": "",
+#     "talkback_source": None
+# }
+#
+# response = requests.post(url, data=data)
+# print(response.status_code)
 
-response = requests.post(url, data=data)
-print(response.status_code)
+
+# ----------------------------------------------------------------------
+
+# Example 2:
+
+def response_2():
+    url_2 = "https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/buzz/posts"
+    data_2 = {
+        "type": "text",
+        "text": "e"
+    }
+    cookies = {
+        'orangehrm': 'emd5197n1nd1ia1g33k90u6lhp'
+    }
+    response = requests.post(url_2, data=data_2, cookies=cookies)
+    return response
+
+
+def performance_test():
+    count = 0
+    while count < 10:
+        response_2()
+        count += 1
+
+
+performance_test()
